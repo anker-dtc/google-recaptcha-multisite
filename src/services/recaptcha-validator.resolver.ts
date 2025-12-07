d14bf6f0dd88782ec6e70e70970f98625473d304
shady.duan <shady.duan@anker-in.com>
2025-12-07 16:46:15 +0800

    feat: add detailed debug logging for secret key resolution

--- src/services/recaptcha-validator.resolver.ts
+++ src/services/recaptcha-validator.resolver.ts
@@ -40,22 +40,29 @@ export class RecaptchaValidatorResolver {
 		// 1. 多站点配置 (sites array)
 		// 2. 全局 SECRET_MAP (GOOGLE_RECAPTCHA_SECRET_MAP)
 		if (siteKey) {
+			this.logger.debug(`[reCAPTCHA] Resolving secret for site key: ${siteKey.substring(0, 20)}...`);
+
 			// 1. 先从 sites 配置中查找
 			if (this.configRef.valueOf.sites) {
 				const siteConfig = this.configRef.valueOf.sites.find(site => site.siteKey === siteKey);
 				if (siteConfig) {
+					this.logger.debug(`[reCAPTCHA] Found in sites config: ${siteConfig.name}`);
 					validator.setCurrentSecretKey(siteConfig.secretKey);
 					return validator;
 				}
+				this.logger.debug(`[reCAPTCHA] Not found in sites config, trying SECRET_MAP...`);
 			}
 
 			// 2. 从全局 SECRET_MAP 中查找 (fallback)
 			if (GOOGLE_RECAPTCHA_SECRET_MAP[siteKey]) {
+				this.logger.debug(`[reCAPTCHA] Found in SECRET_MAP`);
 				validator.setCurrentSecretKey(GOOGLE_RECAPTCHA_SECRET_MAP[siteKey]);
 				return validator;
 			}
 
 			// 如果都找不到，抛出异常
+			this.logger.error(`[reCAPTCHA] No secret found for site key: ${siteKey.substring(0, 20)}...`);
+			this.logger.error(`[reCAPTCHA] Available keys in SECRET_MAP: ${Object.keys(GOOGLE_RECAPTCHA_SECRET_MAP).map(k => k.substring(0, 20) + '...').join(', ')}`);
 			throw new GoogleRecaptchaException(
 				[ErrorCode.MissingInputSecret],
 				`No secret key found for site key: ${siteKey}`
